bce0d13790b77c319afe3d71e11cc0713293c006
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-10 18:44:27 +0100

    re-uses logic

--- app/http/api/data_access_layer.py
+++ app/http/api/data_access_layer.py
@@ -6,20 +6,20 @@ def get_cwd():
     return os.getcwd()
 
 
-def get_users(columns):
-    with open(get_cwd() + "/movies_large/users.csv", mode="r") as csv_file:
+def get_users():
+    return get_data(
+        get_cwd() + "/movies_large/users.csv",
+        ["UserId", "Name"]
+    )
+
+
+def get_data(filename, columns):
+    with open(filename, mode="r") as csv_file:
         csv_rows = csv.DictReader(csv_file, delimiter=';')
         data_sets = []
 
         for row in csv_rows:
             data = dict()
-
-            # TODO: re-use this logic for the other funcs
-            # column[] as param,
-            # for c in column
-            #
-            # data["UserId"] = row["UserId"]
-            # data["Name"] = row["Name"]
 
             for c in columns:
                 data[c] = row[c]
@@ -29,4 +29,4 @@ def get_users(columns):
         return data_sets
 
 
-print(get_users(["UserId", "Name"]))
+print(get_users())
