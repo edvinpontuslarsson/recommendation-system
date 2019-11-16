ee79128cc0ed03701d37d7fde64d7e53ec512c11
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-16 16:26:31 +0100

    euclidian works, see find to three matches

--- server/test.py
+++ server/test.py
@@ -2,6 +2,6 @@ import data_access_layer
 import recommendations
 
 ratings = data_access_layer.get_ratings()
-distance = recommendations.euclidean("7", "1", ratings)
+distance = recommendations.euclidean("7", "5", ratings)
 
 print(distance)
