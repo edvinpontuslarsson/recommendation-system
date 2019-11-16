dbba6152e051ce27dea9a56be785caf54c4e9686
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-16 15:55:26 +0100

    moves client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import BasicForm from './components/BasicForm'
+
+const App = () => {
+  return (
+    <>
+      <BasicForm />
+    </>
+  )
+}
+
+export default App
